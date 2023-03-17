69ee890ffc78cad6f17f483f0fb0d1e28b224d25
Manoj Kumar <95513071+Iamsidar07@users.noreply.github.com>
2023-03-17 16:34:51 +0530

    added bg in card image

--- components/Card.jsx
+++ components/Card.jsx
@@ -12,7 +12,7 @@ const Card = ({ _id, name, photo, prompt, profilePhoto }) => {
         src={photo}
         width={1080}
         height={1350}
-        className={`w-full h-auto object-contain rounded sm:rounded-xl bg-slate-100 ${isLoadingComplete ? "animate-none" : "animate-pulse"}`}
+        className={`w-full h-auto object-contain rounded sm:rounded-xl bg-slate-200 ${isLoadingComplete ? "animate-none" : "animate-pulse"}`}
         alt={prompt}
         placeholder='blur'
         blurDataURL={photo}
@@ -22,14 +22,14 @@ const Card = ({ _id, name, photo, prompt, profilePhoto }) => {
         <p className='text-white text-sm overflow-y-auto prompt'>{prompt}</p>
         <div className="mt-5 flex justify-between items-center gap-2">
           <div className="flex items-center gap-2">
-            <div className='w-7 h-7 rounded-full '>
+            <div className='w-7 h-7 rounded-full bg-slate-100 '>
               
             {profilePhoto &&
               <Image
                 src={profilePhoto}
                 width={40}
                 height={40}
-                className={`w-full h-auto object-cover rounded-full bg-slate-100 ${isLoadingComplete ? "animate-none" : "animate-pulse"}`}
+                className={`w-full h-auto object-cover rounded-full  ${isLoadingComplete ? "animate-none" : "animate-pulse"}`}
                 alt={name}
                 placeholder='blur'
                 blurDataURL={profilePhoto}
